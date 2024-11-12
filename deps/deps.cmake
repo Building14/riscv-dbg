8f0c35a3f792bdff20fad73e93ddd4057c959c1f
Marco Andorno <marco.andorno@cern.ch>
2024-11-12 09:03:00 +0100

    Update common_cells dep

--- deps/deps.cmake
+++ deps/deps.cmake
@@ -19,7 +19,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 
 CPMAddPackage(
     NAME common_cells
-    GIT_TAG v0.0.1
+    GIT_TAG v0.0.4
     GIT_REPOSITORY "https://github.com/HEP-SoC/common_cells.git"
 )
 
